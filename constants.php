610e2099422d76fb97770fe488921396dd7369cd
xelox <93778245+xelox@users.noreply.github.com>
2021-11-05 14:34:16 +0000

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,34 @@
+<?php
+
+    $footer = "
+    <table>
+    <tr style=width:520px>
+		<td style=width:520px><p>Kind Regards,<br/>The Operational Team<br/>P: 0203 5385 727</p>
+			<div style=width:510px>
+				<div style=background:#e6e6e7; width:510px>
+					<img class=style1 src=https://www.cornerstoneremovals.co.uk/logo-email.png />
+						<p style=color:#00357f; padding:0 15px 0 15px; font-family: Arial;font-size:10pt; width:510px >
+							<i>&ldquo;I have used Cornerstone Removals for all jobs in the last two years - they are very professional - trustworthy and always on time.<br>
+							I cannot stress how impressed I am with them and highly recommend Cornerstone Removals for removals.&rdquo;</i>
+							<br>.<br>
+							Alexander Spencer-Churchill
+							</p>
+							<img class=style1 src=https://www.cornerstoneremovals.co.uk/feedback-stars1.png />
+							<img class=style1 src=https://www.cornerstoneremovals.co.uk/NGRS.png />
+							<p style=color:#00357f; padding:0 15px 15px 15px; font-family: Arial;font-size:10pt; width:510px > <b> About Cornerstone Removals</b><br/>
+							Cornerstone Removals is a family-owned company that started years ago<br> with moving a sofa and a fridge from here to there, and now has clients that own castles.
+						</p>
+				</div>
+				<p>
+					<a href=https://www.cornerstoneremovals.co.uk/testimonials/ target=_blank>Testimonials</a> -
+					<a href=https://www.cornerstoneremovals.co.uk/privacy-policy/ target=_blank>Privacy Policy</a> -
+				    <a href=https://www.cornerstoneremovals.co.uk/terms-conditions/ target=_blank>Terms and Conditions</a>
+				</p>
+			</div>
+        </td>
+    </tr>
+    </table>
+    ";
+
+    
+?>
